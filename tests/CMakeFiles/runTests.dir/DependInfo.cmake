d563a0a9d4af492e69d435ef28d2f295601bf9ec
stefanaerospace <stefan.aerospace@gmail.com>
2019-03-06 21:55:11 -0700

    gtest works now, forgot to put a main() in my unit test

--- tests/CMakeFiles/runTests.dir/DependInfo.cmake
+++ tests/CMakeFiles/runTests.dir/DependInfo.cmake
@@ -10,6 +10,7 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/stefan/bin/arduino-1.6.9/hardware/arduino/avr/cores/arduino/inc"
   )
 
 # Targets to which this target links.
--- tests/Drive_test.cpp
+++ tests/Drive_test.cpp
@@ -8,3 +8,8 @@ TEST(ProperGearing, move_me) {
     ASSERT_EQ(1, car.drive_status);
 }
 
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
